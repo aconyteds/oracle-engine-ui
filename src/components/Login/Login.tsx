f47bce11d9de8d56c15cb0733ecab9aedafb1480
Ian Wilson <ianwilson1337@gmail.com>
2025-01-25 11:26:14 -0600

    Converted Login to use Bootstrap

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,119 +1,140 @@
 import React, { useState } from "react";
 import { auth, googleProvider } from "../firebase";
 import {
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  signInWithPopup,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    UserCredential,
 } from "firebase/auth";
-import {
-  Button,
-  Input,
-  VStack,
-  Text,
-  FormControl,
-  Container,
-  Flex,
-  Heading,
-} from "@chakra-ui/react";
+import { Button, Row, Container, Form, Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { setAuthToken } from "../../apolloClient";
+import { useUserContext } from "@context";
 
 export const Login: React.FC = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useUserContext();
 
-  const allowRegistration = import.meta.env.VITE_ALLOW_REGISTRATION === "true";
+    const allowRegistration =
+        import.meta.env.VITE_ALLOW_REGISTRATION === "true";
 
-  const handleLogin = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate("/");
-    } catch (e) {
-      setError("Failed to log in. Please check your credentials.");
-    }
-  };
+    const handleSuccessfulLogin = async (userCredentials: UserCredential) => {
+        const token = await userCredentials.user.getIdToken();
+        setAuthToken(token);
+        setIsLoggedIn();
+        navigate("/");
+    };
 
-  const handleRegister = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      navigate("/");
-    } catch (e) {
-      setError("Failed to register. Please try again.");
-    }
-  };
+    const handleLogin = async () => {
+        try {
+            const userCredentials = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            await handleSuccessfulLogin(userCredentials);
+        } catch (e) {
+            setError("Failed to log in. Please check your credentials.");
+        }
+    };
 
-  const handleGoogleSignIn = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider);
-      navigate("/");
-    } catch (e) {
-      setError("Failed to sign in with Google. Please try again.");
-    }
-  };
+    const handleRegister = async () => {
+        try {
+            const userCredentials = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            await handleSuccessfulLogin(userCredentials);
+        } catch (e) {
+            setError("Failed to register. Please try again.");
+        }
+    };
 
-  const onSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    handleLogin();
-  };
+    const handleGoogleSignIn = async () => {
+        try {
+            const userCredentials = await signInWithPopup(auth, googleProvider);
+            await handleSuccessfulLogin(userCredentials);
+        } catch (e) {
+            setError("Failed to sign in with Google. Please try again.");
+        }
+    };
 
-  return (
-    <Flex justifyContent="center" alignItems="center" height="100vh">
-      <Container>
-        <form onSubmit={onSubmit}>
-          <VStack spacing={4} align="stretch">
-            <Heading textAlign={"center"}>Welcome to Oracle-Engine</Heading>
-            {allowRegistration && (
-              <>
-                <FormControl>
-                  <Text>Email</Text>
-                  <Input
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </FormControl>
-                <FormControl>
-                  <Text>Password</Text>
-                  <Input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </FormControl>
-                {error && <Text color="red.500">{error}</Text>}
-                <Button type="submit" colorScheme="blue" onClick={handleLogin}>
-                  Login
-                </Button>
-                (
-                <Button colorScheme="green" onClick={handleRegister}>
-                  Register
-                </Button>
-                )
-              </>
-            )}
-            (
-            <Button colorScheme="red" onClick={handleGoogleSignIn}>
-              <Flex
-                justifyContent="center"
-                alignItems="baseline"
-                flexDirection="row"
-                width={"100%"}
-                gap={4}
-              >
-                <Text as="span">
-                  <FontAwesomeIcon icon={faGoogle} />
-                </Text>
-                <Text as="span">Sign in with Google</Text>
-              </Flex>
-            </Button>
-            )
-          </VStack>
-        </form>
-      </Container>
-    </Flex>
-  );
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        handleLogin();
+    };
+
+    return (
+        <Row className="h-100 justify-content-center align-items-center">
+            <Col xs="auto">
+                <Form onSubmit={onSubmit} className="row-gap-1">
+                    <Row>
+                        <h3 className="text-center">
+                            Welcome to Oracle-Engine
+                        </h3>
+                    </Row>
+                    {allowRegistration && (
+                        <>
+                            <Form.Group>
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control
+                                    placeholder="Email"
+                                    type="email"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control
+                                    placeholder="Password"
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                />
+                            </Form.Group>
+                            {error && <p className="text-danger">{error}</p>}
+                            <Row className="justify-content-center m-2">
+                                <Col xs="auto">
+                                    <Button
+                                        type="submit"
+                                        variant="primary"
+                                        onClick={handleLogin}
+                                    >
+                                        Login
+                                    </Button>
+                                </Col>
+                                <Col xs="auto">
+                                    <Button
+                                        variant="success"
+                                        onClick={handleRegister}
+                                    >
+                                        Register
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </>
+                    )}
+                    <Row>
+                        <Button variant="dark" onClick={handleGoogleSignIn}>
+                            <Row>
+                                <Col xs="auto">
+                                    <FontAwesomeIcon icon={faGoogle} />
+                                </Col>
+                                <Col>Sign in with Google</Col>
+                            </Row>
+                        </Button>
+                    </Row>
+                </Form>
+            </Col>
+        </Row>
+    );
 };
